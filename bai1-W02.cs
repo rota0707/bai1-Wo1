b20e30fb7a1452de965d37732b4fe1a6b202798b
zorozomeo07 <125963795+zorozomeo07@users.noreply.github.com>
2024-03-03 18:09:28 +0800

    phv
    phv

---
+++ bai1-W02.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai1_W01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Word");
+            
+            DateTime dateTime = DateTime.Now;
+            System.Console.WriteLine("DateTime Now  is: " + dateTime);
+
+            Console.ReadKey();
+
+        }
+    }
+}
